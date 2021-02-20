d6728fc87862629e3ec427f581790fb068e42e77
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-19 21:46:44 -0600

    Add files via upload

---
+++ 05_DIAS_HABILES/DIAS_HABILES/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DIAS_HABILES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+
+            Console.WriteLine("Ingrese un día de la semana: ");
+            dia = Console.ReadLine();
+
+            switch (dia)
+            {
+                case "Lunes":
+                    Console.WriteLine("Es un dia habil");
+                    break;
+                case "Martes":
+                    Console.WriteLine("Es un dia habil");
+                    break;
+                case "Miercoles":
+                    Console.WriteLine("Es un dia habil");
+                    break;
+                case "Jueves":
+                    Console.WriteLine("Es un dia habil");
+                    break;
+                case "Viernes":
+                    Console.WriteLine("Es un dia habil");
+                    break;
+                case "Sabado":
+                    Console.WriteLine("No es un dia habil");
+                    break;
+                case "Domingo":
+                    Console.WriteLine("No es un dia habil");
+                    break;
+                default:
+                    Console.WriteLine("Lo siguiente no es un dia valido >>>>  " + dia);
+                    break;
+            }
+        }
+    }
+}
