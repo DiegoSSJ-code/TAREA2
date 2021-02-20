1fd221ee31c51ed31db01d26a6e16d7772795b75
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-19 21:19:58 -0600

    Add files via upload

---
+++ 01_CARACTER MAYUS/CARACTER MAYUS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CARACTER_MAYUS
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                char characther;
+
+                Console.Write("Por favor ingrese un caracter: ");
+                characther = Convert.ToChar(Console.Read());
+
+                if (char.IsUpper(characther))
+                    Console.WriteLine("\nEl caracter es MAYUSCULA.");
+                else
+                    Console.WriteLine("\nEl caracter no es MAYUSCULA.");
+            }
+        }
+    }
