562fd746ea187d97b10d109fc885044d93ae070e
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-19 21:45:53 -0600

    Add files via upload

---
+++ 04_AÑO_BISIESTO/AÑO_BISIESTO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AÑO_BISIESTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Ingrese el año");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 )
+            {
+                Console.WriteLine("EL AÑO ES BISIESTO");
+            }
+            else
+            {
+                Console.WriteLine("EL AÑO NO ES BISIESTO");
+            }
+        }
+    }
+}
