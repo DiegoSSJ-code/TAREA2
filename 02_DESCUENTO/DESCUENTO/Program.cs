3c63814316d86d637184efe575c1e960fc9813f0
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-19 21:24:29 -0600

    Add files via upload

---
+++ 02_DESCUENTO/DESCUENTO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DESCUENTO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double compra, desc;
+            const double porc = 0.20;
+            Console.WriteLine("\n¿Cuanto fue el subtotal a pagar?");
+            compra = Convert.ToDouble(Console.ReadLine());
+            if (compra > 300)
+            {
+                desc = compra - (compra * porc);
+                Console.WriteLine(\n"El total a pagar es de: " + desc);
+            }
+            else
+            {
+                Console.WriteLine("\nEl total a pagar es de: " + compra);
+            }
+        }
+    }
+}
