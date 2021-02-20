920a0de3b15b63247b48e6f8636c5941b219caf5
DiegoSSJ-code <79240682+DiegoSSJ-code@users.noreply.github.com>
2021-02-19 21:50:35 -0600

    Add files via upload

---
+++ 06_NUMERO_PRIMO/Numero primo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Numero_primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, contador = 0;
+            Console.WriteLine("Digite un número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 1)
+            {
+                Console.WriteLine("No es un numero primo ");
+            }
+            else
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        contador++;
+                    }
+                }
+                if (contador <= 2)
+                {
+                    Console.WriteLine("No es un numero primo ");
+                }
+                else
+                {
+                    Console.WriteLine("Es un numero primo ");
+                }
+
+            }
+        }
+    }
+}
